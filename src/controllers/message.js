b104722d861a6ceaeb520f4ec6dd92a58d1b2627
Bohdan <koder2563@gmail.com>
2023-10-05 18:38:06 +0200

    added send and get new meddage

--- src/controllers/message.js
+++ src/controllers/message.js
@@ -1,16 +1,60 @@
 import { Server } from 'socket.io';
 import User from "../models/User.js";
-
+import Dialog from '../models/Message.js'
 
 export const initSocket = (server, corsOptions) => {
     const io = new Server(server, { cors: corsOptions });
 
     io.on('connection', (socket) => {
-        //console.log('a user connected');
 
-        socket.on('message', (data) => {
-            //console.log('Received message:', data);
+        //get messages
+        socket.on('connectToChat', async (data) => {
+            try {
+                const dialog = await Dialog.findOne({
+                    participants: {
+                        $all: [data.user2, data.user1]
+                    }
+                });
+            
+            
+                if (dialog) {
+                    socket.emit('sendAllMessage', dialog.messages);
+                } 
+
+            } catch (error) {
+                console.error('Ошибка при создании/поиске диалога:', error);
+            }
         });
+
+        //send message
+        socket.on('writeMessage', async (data) => {
+            try {
+                let dialog = await Dialog.findOne({
+                    participants: {
+                        $all: [data.user2, data.user1]
+                    }
+                });
+        
+                if (!dialog) {
+                    dialog = new Dialog({
+                        participants: [data.user2, data.user1],
+                        messages: []
+                    });
+        
+                    await dialog.save();
+                }
+        
+                dialog.messages.push({ sender: data.user1, content: data.text });
+                await dialog.save();
+        
+                socket.emit('sendNewMessage', dialog.messages);
+            } catch (error) {
+                console.error('Ошибка при создании/поиске диалога:', error);
+            }
+        });
+        
+        
+
     });
 
     return io;
